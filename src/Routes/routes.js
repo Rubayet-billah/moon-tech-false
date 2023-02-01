f013200762934b7e523bd938682539b761f50ed9
Rubayet-billah <rubayetbillah909@gmail.com>
2023-02-01 23:58:55 +0600

    header changed

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
+import About from "../Pages/Paths/About";
+import Pricing from "../Pages/Paths/Pricing";
+import Services from "../Pages/Paths/Services";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +13,18 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/services",
+        element: <Services />,
+      },
+      {
+        path: "/pricing",
+        element: <Pricing />,
       },
     ],
   },
