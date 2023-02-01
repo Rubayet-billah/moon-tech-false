9b0a71d9c491a1e39750dd7867519ce5bc15c5dd
Rubayet-billah <rubayetbillah909@gmail.com>
2023-02-01 21:43:28 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import router from "./Routes/routes";
 
 function App() {
   return (
     <div className="App">
-      <h1>Moon Tech</h1>
+      <RouterProvider router={router} />
     </div>
   );
 }
